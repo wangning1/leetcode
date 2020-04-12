52ab8206c1b885b10ce988e65d862a38f7c42c8c
wangning1 <2631015638@qq.com>
2020-04-12 17:02:48 +0800

    增加104、111二叉树最大深度和最小深度

---
+++ 111_Minimum_Depth_of_Binary_Tree/src/TreeNode.java
@@ -0,0 +1,13 @@
+/**
+ * @authour winner
+ * @Date 2020/4/12 16:29
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
