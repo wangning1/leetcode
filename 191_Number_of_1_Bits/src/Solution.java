527a2493c085448a172bad91d128d909f995f12a
ningwang216580 <ningwang216580@sohu-inc.com>
2020-04-17 18:42:26 +0800

    Leetcode 191 统计二进制1的个数 增加解法2

--- 191_Number_of_1_Bits/src/Solution.java
+++ 191_Number_of_1_Bits/src/Solution.java
@@ -7,9 +7,20 @@ public class Solution {
 
     public int hammingWeight(int n) {
         int count = 0;
-        while (n!= 0) {
+        while (n != 0) {
             count++;
             n = n & (n - 1);
+        }
+        return count;
+    }
+
+    public int hamingWeight2(int n) {
+        int count = 0;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                count++;
+            }
+            n = n >> 1;
         }
         return count;
     }
@@ -18,5 +29,9 @@ public class Solution {
         Solution solution = new Solution();
         System.out.println();
         System.out.println(solution.hammingWeight(11));
+        System.out.println(solution.hamingWeight2(11));
+
+        System.out.println(solution.hammingWeight(8));
+        System.out.println(solution.hamingWeight2(8));
     }
 }
