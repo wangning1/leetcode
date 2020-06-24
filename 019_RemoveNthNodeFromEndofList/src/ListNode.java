d9b3dcbf38480726975f0856ca0097b933df7e86
ningwang216580 <2631015638@qq.com>
2020-06-24 17:11:44 +0800

    Add files via upload

---
+++ 019_RemoveNthNodeFromEndofList/src/ListNode.java
@@ -0,0 +1,13 @@
+/**
+ * @Description:
+ * @CreateDate: 2020/6/24 16:38
+ * @Author: wangning
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
