cbdcca735b268a3693daff527c3d3017fdbfcd41
ningwang216580 <2631015638@qq.com>
2021-07-29 18:56:50 +0800

    最佳买卖股票II增加动态规划解法

--- 122_Best_Time_to_Buy_and_Sell_Stock_II/src/Solution.java
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II/src/Solution.java
@@ -13,6 +13,24 @@ public class Solution {
         }
         return maxProfit;
     }
+    
+    //动态规划解法
+        public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[prices.length - 1][0];
+    }
 
     public static void main(String[] args) {
         int[] nums = new int[]{7,6,4,3,1};
