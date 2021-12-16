d6bea1eb1982cf52a9c18876a78946cf34104d80
ningwang216580 <2631015638@qq.com>
2021-12-16 14:26:15 +0800

    LeetCode 27移除元素

---
+++ 027_RemoveElement/src/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right - 1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+
+    public int removeElement1(int[] nums, int val) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+}
