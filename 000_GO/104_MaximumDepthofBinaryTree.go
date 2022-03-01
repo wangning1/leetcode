56e39b24c79a0f2ba324adeb6e6eb0cf6e12a946
ningwang216580 <2631015638@qq.com>
2022-03-01 21:44:58 +0800

    Go语言版LeetCode104二叉树最大深度
    Go语言版LeetCode104二叉树最大深度

---
+++ 000_GO/104_MaximumDepthofBinaryTree.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	return max(leftDepth, rightDepth) + 1
+}
+
+func max(a, b int) int{
+	if(a > b){
+		return a
+	}
+	return b
+}
